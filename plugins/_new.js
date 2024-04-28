ae1b70a95fc9bf3a2cc66cec041f668ca957214f
Ibrahim Adams <162009674+ibrahimaitech@users.noreply.github.com>
2024-04-28 15:05:54 +0300

    Update _new.js

--- plugins/_new.js
+++ plugins/_new.js
@@ -150,7 +150,7 @@ smd(
    filename: __filename
  }, async _0x17bb63 => {
    try {
-     let _0x12a593 = "*BOT RUNNING WELL!*";
+     let _0x12a593 = "*BOT CURRENTLY ACTIVE!*";
      await _0x17bb63.reply(_0x12a593, {
        contextInfo: {
          externalAdReply: {
